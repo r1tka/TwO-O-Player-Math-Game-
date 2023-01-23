e2079208977a8b758061b764eec7fd9b9e12ad7f
Margaryta <margaryta.lanova@gmail.com>
2023-01-23 14:36:37 -0800

    math game

---
+++ main.rb
@@ -0,0 +1,10 @@
+require './player'
+require './game'
+
+player1 = Player.new("Player1")
+player2 = Player.new("Player2")
+
+
+new_game = Game.new(player1, player2)
+
+new_game.play
---
+++ player.rb
@@ -0,0 +1,13 @@
+class Player
+  attr_accessor :live
+
+  def initialize(name)
+    @name = name
+    @live = 3
+  end 
+
+  def to_s
+    @name
+  end
+
+end
